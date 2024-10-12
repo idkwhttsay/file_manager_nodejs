22d3e606f7e61f37aa5fe3902b5dee8a76c8172e
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-10-12 17:51:52 -0400

    feat: detailed info about cpus

--- src/OS.js
+++ src/OS.js
@@ -5,7 +5,20 @@ const printOS = (operation) => {
   if (operation === "--EOL") {
     console.log(os.EOL);
   } else if (operation === "--cpus") {
-    console.table(os.cpus());
+    const cpus = os.cpus();
+    const cpusTable = cpus.map((cpus) => {
+      return {
+        model: cpus.model,
+        speed: cpus.speed,
+        idle: cpus.times.idle,
+        user: cpus.times.user,
+        nice: cpus.times.nice,
+        sys: cpus.times.sys,
+        irq: cpus.times.irq,
+      };
+    });
+
+    console.table(cpusTable);
   } else if (operation === "--homedir") {
     console.log(os.homedir());
   } else if (operation === "--username") {
