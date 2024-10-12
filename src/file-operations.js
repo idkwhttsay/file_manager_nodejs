a97dfd964a9f906b250810a4071b34755cede2d6
idkwhttsay <danil.tatarinov.00@gmail.com>
2024-10-12 16:52:42 -0400

    feat: copy, remove, move

--- src/file-operations.js
+++ src/file-operations.js
@@ -51,6 +51,7 @@ const add = (currentDir, fileName) => {
   });
 };
 
+// TODO: validate paths
 const rename = (currentDir, pathToFile, newFileName) => {
   const fullPathToFile = formAbsolutePath(currentDir, pathToFile);
   const pathToRenamedFile = formAbsolutePath(
@@ -70,4 +71,109 @@ const rename = (currentDir, pathToFile, newFileName) => {
   });
 };
 
-export { cat, add, rename };
+const remove = (currentDir, pathToFile) => {
+  const fullPath = formAbsolutePath(currentDir, pathToFile);
+
+  if (!checkRoot(fullPath) || !checkExistsFile(fullPath)) {
+    invalidInputException();
+    return;
+  }
+
+  fs.rm(fullPath, (error) => {
+    if (error) {
+      operationFailedException();
+      return;
+    }
+
+    successMessage();
+  });
+};
+
+const copy = (currentDir, pathToFile, pathToNewDirectory) => {
+  const fullPathToFile = formAbsolutePath(currentDir, pathToFile);
+  const fullPathToNewDirectory = formAbsolutePath(
+    currentDir,
+    pathToNewDirectory,
+  );
+
+  if (
+    !checkRoot(fullPathToFile) ||
+    !checkExistsFile(fullPathToFile) ||
+    !checkRoot(fullPathToNewDirectory) ||
+    !checkExistsDirectory(fullPathToNewDirectory)
+  ) {
+    invalidInputException();
+    return;
+  }
+
+  const newFilePath = path.join(
+    fullPathToNewDirectory,
+    path.basename(fullPathToFile),
+  );
+
+  const readStream = fs.createReadStream(fullPathToFile);
+  const writeStream = fs.createWriteStream(newFilePath);
+
+  readStream.pipe(writeStream);
+
+  readStream.on("end", () => {
+    successMessage();
+  });
+
+  readStream.on("error", () => {
+    operationFailedException();
+  });
+
+  writeStream.on("error", () => {
+    operationFailedException();
+  });
+};
+
+const move = (currentDir, pathToFile, pathToNewDirectory) => {
+  const fullPathToFile = formAbsolutePath(currentDir, pathToFile);
+  const fullPathToNewDirectory = formAbsolutePath(
+    currentDir,
+    pathToNewDirectory,
+  );
+
+  if (
+    !checkRoot(fullPathToFile) ||
+    !checkExistsFile(fullPathToFile) ||
+    !checkRoot(fullPathToNewDirectory) ||
+    !checkExistsDirectory(fullPathToNewDirectory)
+  ) {
+    invalidInputException();
+    return;
+  }
+
+  const newFilePath = path.join(
+    fullPathToNewDirectory,
+    path.basename(fullPathToFile),
+  );
+
+  const readStream = fs.createReadStream(fullPathToFile);
+  const writeStream = fs.createWriteStream(newFilePath);
+
+  readStream.pipe(writeStream);
+
+  readStream.on("end", () => {
+    fs.rm(fullPathToFile, (error) => {
+      if (error) {
+        operationFailedException();
+        return;
+      }
+
+      successMessage();
+    });
+  });
+
+  readStream.on("error", () => {
+    operationFailedException();
+  });
+
+  writeStream.on("error", () => {
+    operationFailedException();
+  });
+};
+
+export { cat, add, rename, remove, copy, move };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import { calculateHash } from "./hash.js";
 import path from "path";
 import { printOS } from "./OS.js";
 import { compress, decompress } from "./brzip.js";
-import { add, cat, rename } from "./file-operations.js";
-import { printCurrentFolder } from "./errors-and-checks.js";
+import { add, cat, copy, move, remove, rename } from "./file-operations.js";
+import {
+  invalidInputException,
+  printCurrentFolder,
+} from "./errors-and-checks.js";
 
 const login = () => {
   const args = process.argv[2];
@@ -49,6 +52,17 @@ const login = () => {
     } else if (input.slice(0, 2) === "rn") {
       const splitInput = input.split(" ");
       rename(__dirname, splitInput[1].toString(), splitInput[2].toString());
+    } else if (input.slice(0, 2) === "cp") {
+      const splitInput = input.split(" ");
+      copy(__dirname, splitInput[1], splitInput[2]);
+    } else if (input.slice(0, 2) === "rm") {
+      const splitInput = input.split(" ");
+      remove(__dirname, splitInput[1]);
+    } else if (input.slice(0, 2) === "mv") {
+      const splitInput = input.split(" ");
+      move(__dirname, splitInput[1], splitInput[2]);
+    } else {
+      invalidInputException();
     }
 
     printCurrentFolder(__dirname);
